4be73b0bd55b0d26a077e2f04ef5c14329a831b7
kaessam <kaessam17@gmail.com>
2024-05-29 05:11:22 +0300

    feat: Add EF Core and EF SQLite

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a405f72e80c32eed46b327da012b6b2755ccfcdf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe29053e0f91686498a8fbac9941131623c1430")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
